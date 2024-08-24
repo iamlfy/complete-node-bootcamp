5fd82bda7ee415eb7d1dcb21ccabbcc0f3ea6e4a
liufeiyu01 <liufeiyu01@baidu.com>
2024-08-24 17:55:12 +0800

    set html template

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const url = require("url");
+const fs = require('fs');
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+    
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    // Overview
+    if (pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+        res.end(output);
+
+    // About
+    } else if (pathName === '/about') {
+        res.end("About");
+    
+    // API
+    } else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+    
+    // Not Found
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-head': 'hahah'
+        })
+        res.end('<h1>Page not found.</h2>');
+    }
+})
+
+server.listen(9527, "0.0.0.0", () => {
+    console.log("Server is running on port 9527");
+})
